5f8b156af1aa1069630f51d2b0dfe52dbcd70bdb
Carlo Andrea Conte <carloandreaconte@icloud.com>
2016-03-06 21:37:12 -0800

    trying with following tutorial

--- bundle.js
+++ bundle.js
@@ -1,14 +1,16 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"default":[function(require,module,exports){
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-exports.default = function () {
-    console.log("Hello world im the imported shit");
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = {
+    mult: function(x, y) {
+        return x * y;
+    }
+};
+},{}],"APP":[function(require,module,exports){
+var math = require('./math');
+var APP = {
+    run: function() {
+        alert(math.mult(2, 6));
+    }
 };
 
-;
-
-},{}]},{},[]);
+module.exports = APP;
+},{"./math":1}]},{},[]);
